3de3e5a4d5959a45dca1c15cb48dadc4dbae3e56
Junaid Hassan <junaid.hassan@zeikh.com>
2024-08-21 00:38:03 +0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const hasFetched = useRef(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const paymentID = queryParams.get("paymentID");
+    const status = queryParams.get("status");
+
+    console.log("Payment ID:", paymentID);
+    console.log("Status:", status);
+
+    if (paymentID && status && !hasFetched.current) {
+      hasFetched.current = true;
+
+      const data = {
+        status: "success",
+        paymentID: paymentID,
+      };
+
+      const callUpdateAPI = async () => {
+        try {
+          const response = await fetch("https://api.realbdgame.com/api/v1/payment/update-status", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+
+          const result = await response.json();
+
+          const { res, error } = result;
+
+          await fetch("https://api.realbdgame.com/open-app", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ packageName: "com.DefaultCompany.Carrom" }),
+          });
+
+          if (error) {
+            setError(error);
+            console.error("Error:", error);
+          } else {
+            console.log("Response:", res);
+          }
+        } catch (err) {
+          console.error("API call failed:", err);
+        }
+      };
+
+      callUpdateAPI();
+    }
+  }, [location.search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1 className="dot-animate">Payment Completing, wait for a moment</h1>
+      {error && <p>{error}</p>}
     </div>
   );
 }
